48ab3566402b0dab73ec2f6f18370fc930585574
sevinc995 <sevincseferova25@gmail.com>
2025-09-18 09:34:31 +0000

    project

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 "use strict";
 $(document).ready(function () {
-   
+    let myButton = document.querySelector("button");
+    let myHeading = document.querySelector("h1");
+    function setUserName() {
+        const myName = prompt("Please enter your name.");
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+    if (!localStorage.getItem("name")) {
+        setUserName();
+    } else {
+        const storedName = localStorage.getItem("name");
+        myHeading.textContent = `Saytımıza xoş gəlmisən dəyərli, ${storedName}`;
+    }
+    myButton.addEventListener("click", () => {
+        setUserName();
+    });
+    function setUserName() {
+        const myName = prompt("Please enter your name.");
+        if (!myName) {
+            setUserName();
+        } else {
+            localStorage.setItem("name", myName);
+            myHeading.textContent = `Saytımıza xoş gəlmisən dəyərli, ${myName}`;
+        }
+    }
 });
